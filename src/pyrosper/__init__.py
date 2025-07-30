df22e2775715fb7fecc148520c44dfff6963892b
robertleeplummerjr <robertleeplummerjr@gmail.com>
2025-07-30 18:38:49 -0400

    chore: Expose entities for package

--- src/pyrosper/__init__.py
+++ src/pyrosper/__init__.py
@@ -1,6 +1,15 @@
 from .version import __version__
 
-# Import main modules to make them available at package level
+# Import main classes and functions for easy access
+from .base_experiment import BaseExperiment
+from .variant import Variant
+from .symbol import Symbol
+from .user_variant import UserVariant
+from .mock_algorithm import MockAlgorithm
+from .mock_experiment import MockExperiment
+from .pyrosper import Pyrosper, pick
+
+# Import modules for advanced usage
 from . import base_experiment
 from . import variant
 from . import symbol
@@ -10,7 +19,22 @@ from . import mock_experiment
 from . import pyrosper
 
 __all__ = [
+    # Version
     "__version__",
+    
+    # Main classes
+    "BaseExperiment",
+    "Variant", 
+    "Symbol",
+    "UserVariant",
+    "MockAlgorithm",
+    "MockExperiment",
+    "Pyrosper",
+    
+    # Functions
+    "pick",
+    
+    # Modules for advanced usage
     "base_experiment",
     "variant", 
     "symbol",
